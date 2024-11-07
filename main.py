26fbb94311bf35a66a3303740138e22753504c29
augustsletto <augustsletto@gmail.com>
2024-11-07 13:58:14 +0100

    filehandling

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-file = open("my_file.txt")
-contents = file.read()
-
-print(contents)
+with open("my_file.txt", "a") as file:
+    file.write("\nNew text.")
+    
+    
+    
+with open("new_file.txt", "w") as file:
+    file.write("New text.")
